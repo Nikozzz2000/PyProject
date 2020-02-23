85eba2d86ece6a9af90e78e9833d2cd2bb365187
Nikolaj Klimas <Nikozzz2000@googlemail.com>
2020-02-23 23:26:55 +0100

    homework_1.1

--- Homework_1/homework_1.1.py
+++ Homework_1/homework_1.1.py
@@ -1,1 +1,22 @@
-print("test")
+import datetime
+t = datetime.datetime.today().strftime("%Y")
+today = int(t)
+my_age = (today - 1983)
+print("Привет! Давай знакомится, меня завут Нико")
+name = input("А тебя?-")
+print("Привет", name)
+d = input("а кокого числа у тебя день рождения?-")
+m = input("а в каком месяце?-")
+print(m, "-это круто")
+y = input("а в каком году ты родился?-")
+year = int(y)
+age = (today - year)
+s = (my_age - age)
+n = (age - my_age)
+if age < my_age:
+    print("ух ты, это тебе", age, "и ты на", s, "младше меня")
+elif age > my_age:
+    print("ух ты, это тебе", age, "и ты на", n, "старше меня")
+else:
+    print("ух ты, так тебе тоже", age, "и мы ровесники")
+print("Ну ладно, мне пора, а", d, m, "принесу тебе торт с", age, "свечами :)")
