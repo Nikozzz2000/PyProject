f26a30ac97cb488e1e0ce0afb36191f554a9dc2c
Nikolaj Klimas <Nikozzz2000@googlemail.com>
2020-02-24 22:31:37 +0100

    format_homework_1.2

--- Homework_1/homework_1.2.py
+++ Homework_1/homework_1.2.py
@@ -1,17 +1,5 @@
 a = int(input("ввидите ваше число:"))
-h = a // 3600
-m = (a // 60) % 60
-s = a % 60
-if h < 10:
-    h = "0" + str(h)
-else:
-    h = str(h)
-if m < 10:
-    m = "0" + str(m)
-else:
-    m = str(m)
-if s < 10:
-    s = "0" + str(s)
-else:
-    s = str(s)
-print(str(h), "ч. " + str(m), "мин. " + str(s), "сек.")
+hour = a // 3600
+minutes = (a // 60) % 60
+seconds = a % 60
+print("%02d:%02d:%02d" % (hour, minutes, seconds))
