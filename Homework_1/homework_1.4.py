5046ccf097f8793449ea58515b173ebe4a73a807
Nikolaj Klimas <Nikozzz2000@googlemail.com>
2020-02-24 22:56:22 +0100

    format_homework_1.4

--- Homework_1/homework_1.4.py
+++ Homework_1/homework_1.4.py
@@ -5,9 +5,9 @@ if p == 10:
 elif p < 10:
     print(p)
 else:
-    while p > 10:
+    while p > 0:
         n = p % 10
-        p //= 10
+        p = p//10
         if n > i:
             i = n
     print(i)
