b3e7de2dc1f2e4049a0e62debdc760535652f9f2
Nikolaj Klimas <Nikozzz2000@googlemail.com>
2020-02-24 23:31:50 +0100

    format_homework_1.5

--- Homework_1/homework_1.5.py
+++ Homework_1/homework_1.5.py
@@ -1,3 +1,16 @@
 proceeds = int(input("ввидите сумму выручки:"))
 costs = int(input("ввидите сумму издержек:"))
+losses = 0
+income = proceeds - costs
+premium = 0
+if income < 0:
+    losses = costs - proceeds
+    print("Ваши убытки составили", losses, "едениц")
+if income == 0:
+    print("Вы смогли покрыть свои расходы и ничего не заработали")
+if income > 0:
+    workers = int(input("введите количество рабочих:"))
+    premium = income / workers
+    print("Премиальные на человека состовляют", premium, "едениц")
+
 
